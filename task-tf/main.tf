8d0bd56255044b0a467021a19bf8326b036d0c47
srivaibhavi <srivaibhavi.b@kyndryl.com>
2025-07-26 22:04:58 +0530

    win22

--- task-tf/main.tf
+++ task-tf/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "v-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.dev_rg.location
-  resource_group_name = data.azurerm_resource_group.dev_rg.name
+  resource_group_name = variables.resource_group_name
 }
  
 resource "azurerm_subnet" "subnet" {
@@ -238,4 +238,69 @@ resource "azurerm_virtual_machine" "example" {
         sku       = "8-lvm-gen2"
         version   = "latest"
     }
+}
+
+resource "azurerm_virtual_network" "vnetwin" {
+  name                = "vnet-winserver"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnetwin" {
+  name                 = "subnet-winserver"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnetwin.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nicwin" {
+  name                = "nic-winserver"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                    = azurerm_subnet.subnetwin.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id         = azurerm_public_ip.pip.id
+  }
+}
+
+# 5. Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-winserver"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+# 6. Windows Virtual Machine
+resource "azurerm_windows_virtual_machine" "winvm" {
+  name                = "win2022vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_DS2_v2"
+  admin_username      = "azureadmin"
+  admin_password      = "P@ssword1234!"  # Use secrets or key vault in production
+  network_interface_ids = [
+    azurerm_network_interface.nicwin.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "SHC-dev"
+  }
 }
