d68fab68172760889931ae962814482a065204ca
srivaibhavi <srivaibhavi.b@kyndryl.com>
2025-07-28 17:17:26 +0530

    logicapp

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-cp-srivaibhavi-b"
-    storage_account_name  = "vstoreaccount"     # must be globally unique
+    storage_account_name  = "vstoreaccount"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ resource "azurerm_key_vault" "win_kv" {
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id  # your own identity to create the secret
+    object_id = data.azurerm_client_config.current.object_id
 
     secret_permissions = ["Get", "List", "Set"]
   }
