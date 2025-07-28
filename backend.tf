3ba11c7b50cd89ccc0646960b7217e1d15a2e27d
vaibs-hub-sub <srivaibhavibalasubramanian@gmail.com>
2025-07-28 11:14:40 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "rg-cp-srivaibhavi-b"
-    storage_account_name  = "vstoreaccount"     # must be globally unique
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
